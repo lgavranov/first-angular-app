3b4b9933615a8a33eb60240db2df72f314a62602
lgavranovic <luka.gavranovic@easybusy.net>
2026-07-06 13:07:05 +0200

    output example

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, input, Input, signal} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import { DUMMY_USERS} from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,13 +10,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
+    this.select.emit(this.id);
   }
 }
